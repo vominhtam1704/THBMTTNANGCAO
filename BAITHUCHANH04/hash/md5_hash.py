584d2b36312d4173a17b1c441ad7483661bd2477
vominhtam1704 <tam18042021@gmail.com>
2025-05-23 11:25:59 +0700

    bai4

---
+++ BAITHUCHANH04/hash/md5_hash.py
@@ -0,0 +1,55 @@
+def left_rotate(value, shift):
+    return ((value << shift) | (value >> (32 - shift))) & 0xFFFFFFFF
+
+def md5(message):
+    # Khởi tạo các biến ban đầu
+    a = 0x67452301
+    b = 0xEFCDAB89
+    c = 0x98BADCFE
+    d = 0x10325476
+
+    # Tiền xử lý chuỗi văn bản
+    original_length = len(message)
+    message += b'\x80'
+    while len(message) % 64 != 56:
+        message += b'\x00'
+    message += original_length.to_bytes(8, 'little')
+
+    # Chia chuỗi thành các block 512-bit
+    for i in range(0, len(message), 64):
+        block = message[i:i+64]
+        words = [int.from_bytes(block[j:j+4], 'little') for j in range(0, 64, 4)]
+        aa, bb, cc, dd = a, b, c, d
+
+        # Vòng lặp chính của thuật toán MD5
+        for j in range(64):
+            if j < 16:
+                f = (b & c) | ((~b) & d)
+                g = j
+            elif j < 32:
+                f = (d & b) | ((~d) & c)
+                g = (5 * j + 1) % 16
+            elif j < 48:
+                f = b ^ c ^ d
+                g = (3 * j + 5) % 16
+            else:
+                f = c ^ (b | (~d))
+                g = (7 * j) % 16
+
+            temp = d
+            d = c
+            c = b
+            b = (b + left_rotate((a + f + 0x5A827999 + words[g]) & 0xFFFFFFFF, 3)) & 0xFFFFFFFF
+            a = temp
+
+        a = (a + aa) & 0xFFFFFFFF
+        b = (b + bb) & 0xFFFFFFFF
+        c = (c + cc) & 0xFFFFFFFF
+        d = (d + dd) & 0xFFFFFFFF
+
+    return '{:08x}{:08x}{:08x}{:08x}'.format(a, b, c, d)
+
+# Thử nghiệm
+input_string = input("Nhập chuỗi cần băm: ")
+md5_hash = md5(input_string.encode('utf-8'))
+print("MD5 Hash:", md5_hash)
